a35b2fdc97e61bc977fee55a07022a74d597bf08
Emanuel Rew <emanuel.rew@berkeley.edu>
2020-08-16 13:34:23 -0700

    Updates

---
+++ precache-manifest.851f18335c8b8b62e34e94ed1df88431.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00fb15bd98dc3343fd0597ea1b1e2491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0bace7ed6e17ea157cf",
+    "url": "/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "a6ffd72397797438e580",
+    "url": "/static/js/2.0c8b0cbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c8b0cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0bace7ed6e17ea157cf",
+    "url": "/static/js/main.9ab6161e.chunk.js"
+  },
+  {
+    "revision": "4e298d7ae7623fec8b7e",
+    "url": "/static/js/runtime-main.b85d870a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.36ba11838355607dda295aac4f0674b9.js"
+  "/precache-manifest.851f18335c8b8b62e34e94ed1df88431.js"
 );
 
 self.addEventListener('message', (event) => {
