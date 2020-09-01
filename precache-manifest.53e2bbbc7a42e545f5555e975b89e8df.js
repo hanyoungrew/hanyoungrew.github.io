6915b9bbc18991f220e8f8b596285c5b7838130d
Emanuel Rew <hanyoungrew@gmail.com>
2020-09-01 00:51:08 -0700

    Updates

---
+++ precache-manifest.53e2bbbc7a42e545f5555e975b89e8df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1913b6ccb498e4005203873f1cc3cb2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62574ae0e2488d81adb4",
+    "url": "/static/js/2.b36d6348.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b36d6348.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9425bab871d8f48f8436",
+    "url": "/static/js/main.551c2f50.chunk.js"
+  },
+  {
+    "revision": "4e298d7ae7623fec8b7e",
+    "url": "/static/js/runtime-main.b85d870a.js"
+  }
+]);
